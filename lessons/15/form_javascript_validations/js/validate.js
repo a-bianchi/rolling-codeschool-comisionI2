fdf90cb7ccb076edf14197b7e3507f9437b82a5b
Alejo <alejobianchi@gmail.com>
2020-07-14 21:35:31 -0300

    clase 16

--- lessons/15/form_javascript_validations/js/validate.js
+++ lessons/15/form_javascript_validations/js/validate.js
@@ -1,21 +1,33 @@
 function validateform() {
   var name = document.myform.name.value;
   var password = document.myform.password.value;
+  var email = document.myform.email.value;
+  var edad = document.myform.edad.value;
+  var error = document.getElementById("error");
 
-  if (name == null || name == "") {
-    alert("Nombre no puede estar vacio");
-    return false;
-  } else if (password.length < 6) {
-    alert("El password no puede ser menor que 6 caracteres");
+  error.textContent = "";
+
+  if (name == "") {
+    error.textContent = "Nombre no puede estar vacio";
     return false;
   }
-}
 
-function action() {
-  const name = document.getElementById("name");
-  const span = document.getElementById("error");
-  console.log(name.value);
-  span.textContent = "Soy un error";
+  if (password.length < 6) {
+    error.textContent = "El password no puede ser menor que 6 caracteres";
+    return false;
+  }
+
+  if (email == "") {
+    error.textContent = "El email no tiene que ser vacio";
+    return false;
+  }
+
+  // if (parseInt(edad) === NaN) {
+  //   error.textContent = "El campo edad tiene que ser tipo numero";
+  //   return false;
+  // }
+
+  error.textContent = "";
 }
 
 /// document.getElementById("name")
---
+++ lessons/16/js/practica.js
@@ -0,0 +1,67 @@
+// Ejercicios Javascript Variables
+
+// 1) Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+//alert("un mensaje");
+
+// 2) Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+// document.write("Hello Word </br> Hola salte");
+// document.write("<p> Hello Word 2</p>");
+// document.write("<h1> Soy un h1</h1>");
+
+// 3) Lo dejo a tu criterio
+
+// 4) Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// var nombre = prompt("Nombre de usuario?");
+// document.write(nombre);
+
+// 5) Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// var numero1 = prompt("Ingrese un numero:");
+// var numero2 = prompt("Ingrese un numero:");
+// document.write(parseInt(numero1) + parseInt(numero2));
+
+// 9) Escribe un programa que pida una frase y escriba las vocales que aparecen
+// Resuleto clase 17/7
+// Nota: Tener en cuenta la función length y substr.
+
+// ???????
+
+// Ejercicios Javascript bucles(loops)
+
+// 1) Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+// var edad = prompt("Edad:");
+// if (parseInt(edad) >= 18) {
+//   alert("Ya puede conducir");
+// } else {
+//   alert("No es un numero valido");
+// }
+
+// 2) Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+// Evaluar en casa!!!
+// var nota = parseInt(prompt("Ingrese nota:"));
+
+// if (nota > 10 || nota < 0) {
+//   alert("Numero erroneo!!!");
+// }
+
+// switch (nota) {
+//   case 1:
+//   case 2:
+//   case 0:
+//     alert("Muy deficiente");
+//     break;
+//   case (3, 4):
+//     alert("Insuficiente");
+//     break;
+//   case (5, 6):
+//     alert("Suficiente");
+//     break;
+// }
+
+// 6) Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+// Resuleto clase 17/7
+// var n = 0;
+// var numeroQueseyo = 0;
+// for (var x = 0; x <= 30; x++) {
+//   n++;
+//   numeroQueseyo;
+// }
