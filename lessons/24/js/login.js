c882d7e042f3e0256a6c46481d9a18fbf97dfab0
Alejo <alejobianchi@gmail.com>
2020-08-13 21:43:25 -0300

    clase 24

---
+++ lessons/24/js/login.js
@@ -0,0 +1,78 @@
+// Login
+console.log("Se cargo el archivo login.js");
+
+// Clase Usuario
+class Usuario {
+  constructor(username, password) {
+    this._username = username;
+    this._password = password;
+  }
+
+  get username() {
+    return this._username;
+  }
+
+  static validarUsername(username) {
+    if (username.length > 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static validarPassword(password) {
+    // password = "1234@"
+    // Metodo serach("string") busca un string en otro string
+    // Si lo encuentra devuelve la posicion y si no lo encuentra devuelve -1
+    if (password.search("@") === -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  existeUsuario() {
+    console.log("Entre");
+    if (this._username === "Alejo" && this._password === "1234") {
+      console.log("Entre");
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+function checkUsername() {
+  const username = document.getElementById("inputUsername").value;
+  // Es equivalente => if(!Usuario.validarUsername(username))
+  if (Usuario.validarUsername(username) === false) {
+    alert("El nombre de usuario debe tener mas de 3 caracteres!!!");
+  }
+}
+
+function checkPassword() {
+  const password = document.getElementById("inputPassword").value;
+  // Es equivalente => if(!Usuario.validarUsername(username))
+  if (Usuario.validarPassword(password) === false) {
+    alert("El password no debe contener @!!!");
+  }
+}
+
+const evento = document
+  .getElementById("formLogin")
+  .addEventListener("submit", function (e) {
+    const username = document.getElementById("inputUsername").value;
+    const password = document.getElementById("inputPassword").value;
+
+    const usuario = new Usuario(username, password);
+
+    if (usuario.existeUsuario()) {
+      console.log("El usuario es correcto!!!");
+      window.location =
+        "http://127.0.0.1:5500/lessons/24/agregar-producto.html";
+    } else {
+      alert("Usuario no existe!!!");
+    }
+
+    e.preventDefault();
+  });
